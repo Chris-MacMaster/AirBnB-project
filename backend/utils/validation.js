cdfb4396b7f99f8c7dcd133a8c95493199793a8f
christopher <christopher.macmaster@gmail.com>
2023-01-20 15:48:14 -0800

    Add User input validation on user login backend endpoint

--- backend/utils/validation.js
+++ backend/utils/validation.js
@@ -25,6 +25,19 @@ const handleValidationErrors = (req, _res, next) => {
 
 
 
+// const validateLogin = [
+//     check('credential')
+//         .exists({ checkFalsy: true })
+//         .notEmpty()
+//         .withMessage('Please provide a valid email or username.'),
+//     check('password')
+//         .exists({ checkFalsy: true })
+//         .withMessage('Please provide a password.'),
+//     handleValidationErrors
+// ];
+
+
+
 
 module.exports = {
     handleValidationErrors
