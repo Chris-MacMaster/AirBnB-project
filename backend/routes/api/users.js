59a0c5ea3231c603e68d742aa2a2dc3c4ad691a3
christopher <christopher.macmaster@gmail.com>
2023-01-20 15:53:22 -0800

    Add User input validation on user signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -7,7 +7,29 @@ const express = require('express')
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
+
+const validateSignup = [
+    check('email')
+        .exists({ checkFalsy: true })
+        .isEmail()
+        .withMessage('Please provide a valid email.'),
+    check('username')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 4 })
+        .withMessage('Please provide a username with at least 4 characters.'),
+    check('username')
+        .not()
+        .isEmail()
+        .withMessage('Username cannot be an email.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .isLength({ min: 6 })
+        .withMessage('Password must be 6 characters or more.'),
+    handleValidationErrors
+];
 
 
 const router = express.Router();
@@ -16,6 +38,7 @@ const router = express.Router();
 // Sign up
 router.post(
     '/',
+    validateSignup,
     async (req, res) => {
         const { email, password, username } = req.body;
         const user = await User.signup({ email, username, password });
@@ -28,21 +51,20 @@ router.post(
     }
 );
 
-//LzcrBhkl-qpfqoN7BTXIX-6oNBU4eNi_WrYQ
+//LWA30rl6-TYF1ANCMKbhGpwx8yXrdO90-_no
 
 // fetch('/api/users', {
 //     method: 'POST',
 //     headers: {
 //         "Content-Type": "application/json",
-//         "XSRF-TOKEN": `LzcrBhkl-qpfqoN7BTXIX-6oNBU4eNi_WrYQ`
+//         "XSRF-TOKEN": `LWA30rl6-TYF1ANCMKbhGpwx8yXrdO90-_no`
 //     },
 //     body: JSON.stringify({
-//         email: 'spidey@spider.man',
-//         username: 'Spidey',
-//         password: 'password'
+//         email: 'haaag@email.com',
+//         username: '',
+//         password: 'aaa'
 //     })
 // }).then(res => res.json()).then(data => console.log(data));
-
 
 
 module.exports = router;
