523a2f93b727f00c670776fc64d65e8b1feaa4dc
christopher <christopher.macmaster@gmail.com>
2023-02-10 10:21:34 -0800

    doNavigationCSS

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -56,6 +56,17 @@ export const signup = (user) => async (dispatch) => {
 };
 
 
+//phase 3 logout
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
+
+
+
 
 const initialState = { user: null };
 
