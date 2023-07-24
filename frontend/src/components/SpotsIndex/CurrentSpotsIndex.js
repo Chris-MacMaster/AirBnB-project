ae419c266b176011fe134a27fbb4d92590bb4c40
chrismacmaster <christopher.macmaster@gmail.com>
2023-07-23 18:36:03 -0700

    current spot cleanup

--- frontend/src/components/SpotsIndex/CurrentSpotsIndex.js
+++ frontend/src/components/SpotsIndex/CurrentSpotsIndex.js
@@ -1,19 +1,15 @@
 import { useSelector, useDispatch } from "react-redux";
-// import { Link } from "react-router-dom"
 
-// import SpotIndexItem from "./SpotIndexItem";
 import { useEffect } from "react";
 import { fetchCurrentSpots } from "../../store/spot";
 import { actionResetReviews } from "../../store/review";
 
 
 import "./SpotIndex.css"
-import CurrentSpotItem from "./CurrentSpotItem";
 import SpotIndexItem from "./SpotIndexItem";
 
 
 import { useHistory } from "react-router-dom"
-import OpenModalButton from "../OpenModalButton/index"
 import DeleteSpotModal from "./DeleteSpotModal"
 
 import "./CurrentSpotsIndex.css"
@@ -27,15 +23,10 @@ function CurrentSpotsIndex() {
     const history = useHistory()
 
     const spotState = useSelector(state => state.spots)
-    // console.log("spot State", spotState)
-    // console.log(spotState["allSpots"])
-    // console.log("AllSpots", spotState.allSpots)
     const spots = Object.values(spotState.allSpots)
-    // console.log(spots)
     const user = useSelector(state => state.session.user)
 
 
-    //should dispatch on inital render
     useEffect(() => {
         dispatch(fetchCurrentSpots())
         dispatch(actionResetReviews())
@@ -47,9 +38,6 @@ function CurrentSpotsIndex() {
     }
 
     const handleUpdate = (spotId) => {
-        // e.preventDefault()
-        // console.log("update button test")
-        // console.log("SPOT ID", spotId)
         if (user) {
             history.push(`/spots/manage/${spotId}`)
         }
@@ -83,15 +71,9 @@ function CurrentSpotsIndex() {
 
             </div>
         </div>
-        //also note the list, we map over our data passed in from prop,
-        //at each obj, we render link to the url with that obj id along with its name
     )
 }
 
 export default CurrentSpotsIndex;
 
-
-
-
-//store = { session: {}, spots: { allSpots: { [spotId]: { spotData, }, optionalOrderedList: [], }, singleSpot: { spotData, SpotImages: [imagesData], Owner: { ownerData, }, }, }, reviews: { spot: { [reviewId]: { reviewData, User: { userData, }, ReviewImages: [imagesData], }, optionalOrderedList: [], }, user: { [reviewId]: { reviewData, User: { userData, }, Spot: { spotData, }, ReviewImages: [imagesData], }, optionalOrderedList: [], }, }, bookings: { user: { [bookingId]: { bookingData, Spot: { spotData, }, }, optionalOrderedList: [], }, spot: { [bookingId]: { bookingData, }, optionalOrderedList: [], }, }, };
 
